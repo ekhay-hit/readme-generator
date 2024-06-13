13169b9b88d397b4709dee08656e09d30f80ffdf
Houssine <ekhay1@outlook.com>
2024-06-12 22:28:11 -0400

    added logic for prompt input

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-inquirer
-  .prompt([
-    {
-      message: 'Press ENTER to continue...',
-      name: 'enterKey'
-    }
-  ]);
+inquirer.prompt([
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "userName",
+  },
+  {
+    type: "input",
+    message: "What is your project name?",
+    name: "projectName",
+  },
+
+  {
+    type: "input",
+    message: "Please write a shot discription of your project?",
+    name: "description",
+  },
+  {
+    type: "list",
+    message: "What license are you using for your project",
+    name: "license",
+    choices: ["MIT", "APACHE 2.0", "BSD 3", "None"],
+  },
+
+  {
+    type: "input",
+    message: "What is the usage of the project?",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "What installation required?",
+    name: "installation",
+  },
+]);
